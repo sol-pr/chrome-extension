0593a13c24e684b300fb022d7d02badd6176e6e5
Buğra Okumuş <bugra.okumus@d-teknoloji.com.tr>
2024-09-04 21:49:37 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="w-full flex justify-center items-center h-screen">
+        <h1 className="text-blue-500 text-5xl font-bold">Hello World! </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
